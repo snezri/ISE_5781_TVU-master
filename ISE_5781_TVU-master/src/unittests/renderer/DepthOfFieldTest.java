5c7511fb6b61b04879084dc14261acc41b92223d
snezri <saraamouia13@gmail.com>
2021-06-18 17:14:22 +0300

    project 1 depth of field begun

---
+++ ISE_5781_TVU-master/src/unittests/renderer/DepthOfFieldTest.java
@@ -0,0 +1,49 @@
+package unittests.renderer;
+
+import Geometries.Sphere;
+import Primitives.Color;
+import Primitives.Material;
+import Primitives.Point3D;
+import Primitives.Vector;
+import elements.Camera;
+import elements.DirectionalLight;
+import org.junit.Test;
+import renderer.ImageWriter;
+import renderer.RayTracerBasic;
+import renderer.Render;
+import scene.Scene;
+
+public class DepthOfFieldTest {
+    @Test
+    public void depthOfFieldTest3() {
+
+        Scene scene3 = new Scene("basic depth of field test");
+
+        Camera camera3 = new Camera(new Point3D(0,10,500),new Vector(0,0,-1),new Vector(0,1,0))
+                .setDistance(450)
+                .setViewPlaneSize(30,30)
+                .set_depthOfField(50)
+                .set_dOFRadius(3);
+
+        Material mat = new Material().setKd(0.5).setkT(0.1).setkR(0.8).setKs(0.5).setnShininess(500);
+
+        scene3.lights.add(new DirectionalLight(new Color(300,300,300), new Vector(-1,-1,-1)));
+
+        scene3.geometries.add(
+                new Sphere(3, new Point3D(6,12,-100)).setEmission(new Color(180,0,0)).setMaterial(mat),
+                new Sphere(3, new Point3D(3,9,-50)).setEmission(new Color(180,200,0)).setMaterial(mat),
+                new Sphere(3, new Point3D(0,6,0)).setEmission(new Color(0,0,180)).setMaterial(mat),
+                new Sphere(3, new Point3D(-3,3,50)).setEmission(new Color(0,180,0)).setMaterial(mat),
+                new Sphere(3, new Point3D(-6,0,100)).setEmission(new Color(0,180,180)).setMaterial(mat)
+        );
+
+        Render render = new Render() //
+                .setImageWriter(new ImageWriter("depth", 500, 500)) //
+                .setCamera(camera3) //
+                .setRayTracerBase(new RayTracerBasic(scene3))
+                .setGridParams(8);
+        render.renderImageWithDepthOfField();
+        render.writeToImage();
+
+    }
+}
